63d245003198f293f7f0f8c4697f4f1952dabdd9
Jairo Suarez <jasuarezpr@gmail.com>
2017-05-28 17:30:27 -0500

    adding an index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Agent from './Agent';
+import Event from './Event';
+import EventGrabberTuple from './EventGrabberTuple';
+import Grabber from './Grabber';
+import GrabberObject from './GrabberObject';
+import InputHandler from './InputHandler';
+import Shortcut from './Shortcut';
+
+/**
+ */
+const bias = {
+  Agent,
+  Event,
+  EventGrabberTuple,
+  Grabber,
+  GrabberObject,
+  InputHandler,
+  Shortcut,
+};
+
+export default bias;
