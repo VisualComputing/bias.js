0f7aa01132f14808e2c3089d0971f3d1efc3489e
Sebastian Chaparro <sechaparroc@unal.edu.co>
2017-12-15 18:14:35 -0500

    GrabberObject has been removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Agent from './Agent';
 import Event from './Event';
 import event from './event/index';
 import EventGrabberTuple from './Tuple';
-import GrabberObject from './GrabberObject';
 import InputHandler from './InputHandler';
 import Shortcut from './Shortcut';
 
@@ -13,7 +12,6 @@ const bias = {
   Event,
   event,
   EventGrabberTuple,
-  GrabberObject,
   InputHandler,
   Shortcut,
 };
